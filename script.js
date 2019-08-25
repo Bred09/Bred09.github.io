6fc5977b027c2032f9a9f33c1b6fb76122842234
Bred09 <bred.king@bk.ru>
2019-08-25 21:46:39 +0500

    fasted close button and change open/close modile-menu speed

--- script.js
+++ script.js
@@ -1,29 +1,8 @@
-// document.querySelector('.btn-show').onclick = function() {
-//   var mobileMenu = document.querySelector('.mobile-menu');
-//   mobileMenu !== null && b("show").removeClass(form, d)
-// }
-// document.querySelector('.btn-show').onclick = function() {
-//   var mobileMenu = document.querySelector('.mobile-menu');
-//   mobileMenu.addClass('show');
-// }
-
-
-
-var btnShow = document.querySelector('.btn-show');
 var mobileMenu = document.querySelector('.mobile-menu');
-
-btnShow.onclick = function () {
+document.querySelector('.btn-show').onclick = () => {
 	mobileMenu.classList.add('show');
-
-
-	setTimeout(f1, 3000);
 };
 
-function f1() {
-	var btnClose = document.querySelector('.btn-close');
-
-	btnClose.onclick = function () {
-		mobileMenu.classList.remove('show');
-		btnShow.classList.remove('close');
-	}
-}
+document.querySelector('.btn-close').onclick = () => {
+	mobileMenu.classList.remove('show');
+};
