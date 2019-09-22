ac5875c7268629ddbcd0af4467a24028ae046d2b
Bred09 <bred.king@bk.ru>
2019-09-22 19:05:15 +0500

    add animation

--- interface/script.js
+++ interface/script.js
@@ -6,3 +6,17 @@ document.querySelector('.btn-show').onclick = () => {
 document.querySelector('.btn-close').onclick = () => {
 	mobileMenu.classList.remove('show');
 };
+
+
+
+var findInputShow = document.querySelector('.find-i');
+var find = document.querySelector('.find').onclick = () => {
+	var find = document.querySelector('.find');
+	findInputShow.classList.add('f-show');
+	find.style.right = '180px';
+	find.style.transition = 'right 1s';
+};
+
+document.querySelector('.btn-close').onclick = () => {
+	mobileMenu.classList.remove('show');
+};
